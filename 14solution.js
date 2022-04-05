f61e690304ec12a0d73d781012972dde7091202c
stipriausias <domantas987654@gmail.com>
2022-04-05 17:39:31 +0300

    new solution

---
+++ 14solution.js
@@ -0,0 +1,8 @@
+function orderFood(list) {
+    // thank you for checking out the Coding Meetup kata :)
+    const obj={}
+  
+  list.map(v=>obj[v.meal]=obj[v.meal]?obj[v.meal]+1:1)
+  
+  return obj
+  }
