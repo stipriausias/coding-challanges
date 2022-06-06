60b9ce7bbec6e28f78ccb724ec2f4d9381251208
Domantas Krilavicius <domantas987654@gmail.com>
2022-06-06 20:57:29 +0200

    new solution

--- evensTimesLast.js
+++ evensTimesLast.js
@@ -1,4 +1,4 @@
 function evenLast(numbers) {
-    // Good luck.
+    // Good luck..
     return numbers.reduce((sum, element, index) => index % 2 ? sum : sum + element, 0) * numbers[numbers.length - 1] || 0
 }
