5ea779b9b0f55655339f3c936d445ce3f77ba980
Domantas Krilavicius <domantas987654@gmail.com>
2022-06-25 08:59:34 +0200

    new solution

---
+++ complementaryDNA.js
@@ -0,0 +1,11 @@
+function DNAStrand(dna){
+    let sequence = {
+      "A": "T",
+      "T": "A",
+      "G": "C",
+      "C": "G"
+    }
+    return dna.replace(/A|T|G|C/g, function(matched){
+      return sequence[matched];
+    });
+  }
