b35d319a8b65a73aa3f35ecd35c2ed7d185455c3
Domantas Krilavicius <domantas987654@gmail.com>
2022-07-31 14:35:18 +0200

    new solution

--- oddEvenString.js
+++ oddEvenString.js
@@ -1,6 +1,6 @@
 function sortMyString(S) {
     // your code here
     return S.split('').filter((element, index) => index % 2 === 0).join('') + ' ' + S.split('').filter((element, index) => index % 2 !== 0).join('')
-    // good
+    // good solution
     
 }
