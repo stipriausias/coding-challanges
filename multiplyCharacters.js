7ebb25b6fb96af0564a0495f6d2324ce080bf563
Domantas Krilavicius <domantas987654@gmail.com>
2022-08-06 19:06:32 +0200

    new solution

--- multiplyCharacters.js
+++ multiplyCharacters.js
@@ -1,5 +1,5 @@
 function spam(number) {
-    //TODO: Not returning the expected value....
+    //TODO: Not returning the expected value.....
     let answer = 'hue'
     return answer.repeat(number)
 }
