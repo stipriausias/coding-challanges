f182313d939f582fe1236a7749ef7ea2ec9160e6
Domantas Krilavicius <domantas987654@gmail.com>
2022-08-08 19:35:09 +0200

    new solution

--- charCodeCalculator.js
+++ charCodeCalculator.js
@@ -5,6 +5,7 @@ function calc(x){
     let total2 = x.split('').map((element, index) => element.charCodeAt(0)).join('').split('').map((element) => element.replace('7','1')).reduce((sum, element) => sum + Number(element), 0);
     let total1 =  x.split('').map((element, index) => element.charCodeAt(0)).join('').split('').reduce((sum, element) => sum + Number(element), 0);
      return total1 - total2
+    //  we did it
   }
 
   console.log(calc(x))
