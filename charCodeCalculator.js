a5ca6fe9f911d810194cbd16e3edac8bce0583b8
Domantas Krilavicius <domantas987654@gmail.com>
2022-08-09 19:34:27 +0200

    new solution

---
+++ charCodeCalculator.js
@@ -0,0 +1,10 @@
+
+let x = 'abcdef'
+
+function calc(x){
+    let total2 = x.split('').map((element, index) => element.charCodeAt(0)).join('').split('').map((element) => element.replace('7','1')).reduce((sum, element) => sum + Number(element), 0);
+    let total1 =  x.split('').map((element, index) => element.charCodeAt(0)).join('').split('').reduce((sum, element) => sum + Number(element), 0);
+     return total1 - total2
+  }
+
+  console.log(calc(x))
