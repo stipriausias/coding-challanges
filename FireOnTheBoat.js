8c624785a7727d09baa4b7cc0ef063a45e9b83ff
stipriausias <domantas987654@gmail.com>
2022-09-17 14:01:32 +0300

    new solution

--- FireOnTheBoat.js
+++ FireOnTheBoat.js
@@ -1,4 +1,4 @@
 function fireFight(s){
     return s.split(' ').map((element, index) => element.replace('Fire','~~')).join(' ')
-    // not the best solution but atleast it's good soluti
+    // not the best solution but atleast it's good solutio
   }
