91ca4aff359076b44a9945b501f2bdcfe45e5441
stipriausias <domantas987654@gmail.com>
2022-09-25 15:12:25 +0300

    new solution

--- 4solution.js
+++ 4solution.js
@@ -1,4 +1,5 @@
 function getFirstPython(list) {
   const dev = list.find(x => x.language === "Python")
   return dev ? `${dev.firstName}, ${dev.country}` : "There will be no Python developers."
+  // nice
 }
