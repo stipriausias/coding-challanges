59a01bdb65ef753fe8ad8df8129e4fc2c6e5ef11
stipriausias <domantas987654@gmail.com>
2022-09-29 15:09:37 +0300

    new solution

---
+++ lengthTwoValues.js
@@ -0,0 +1,12 @@
+function alternate(n, firstValue, secondValue){
+    // your code
+    let lapas = [];
+    for (let i = 1; i <= n; i++){
+      if(i % 2 != 0){
+        lapas.push(firstValue);
+      } else {
+        lapas.push(secondValue);
+      }
+    }
+    return lapas;
+  }
