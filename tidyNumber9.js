f2259fe0046db748a08d10c5ecd3d2d8da6a2418
stipriausias <domantas987654@gmail.com>
2022-10-19 21:20:51 +0300

    new solution

--- tidyNumber9.js
+++ tidyNumber9.js
@@ -1,5 +1,5 @@
 function tidyNumber(n){
     //your code here
     return n.toString().split('').map(element => Number(element)).every((el, ind, arr) => !ind || el >= arr[ind - 1]);
-    // good solution
+    // good solutions
   }
